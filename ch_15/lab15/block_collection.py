a46b0df7e0ab89d753046cf05829f6d5bd3bef58
Josh S <jostricklin@gmail.com>
2014-05-15 02:18:02 -0500

    chapter 14 complete

--- ch_15/lab15/block_collection.py
+++ ch_15/lab15/block_collection.py
@@ -10,6 +10,7 @@ Explanation video: http://youtu.be/4W2AqUetBi4
 """
 import pygame
 import random
+from my_classes import *
  
 # Define some colors
 BLACK = (  0,   0,   0)
@@ -18,29 +19,6 @@ RED   = (255,   0,   0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
  
-class Block(pygame.sprite.Sprite):
-    """
-    This class represents the ball.
-    It derives from the "Sprite" class in Pygame.
-    """
- 
-    def __init__(self, filename): 
-        # Call the parent class (Sprite) constructor
-        pygame.sprite.Sprite.__init__(self)
- 
-        # Create an image of the block, and fill it with a color.
-        # This could also be an image loaded from the disk.
-        self.image = pygame.image.load(filename).convert()
-
-        # set background to be transparent
-        self.image.set_colorkey(WHITE)
-
-        # Fetch the rectangle object that has the dimensions of the image
-        # image.
-        # Update the position of this object by setting the values
-        # of rect.x and rect.y
-        self.rect = self.image.get_rect()
-
 class Player(pygame.sprite.Sprite):
     """ The class is the player-controlled sprite."""
 
